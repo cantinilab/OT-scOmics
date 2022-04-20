42041af4558a4b6984620d0bd1668711724b67b8
Geert-Jan Huizing <huizing@ens.fr>
2022-04-20 17:40:01 +0200

    doc

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -7,6 +7,8 @@ import anndata as ad
 class TestOTscomics(unittest.TestCase):
 
     def setUp(self) -> None:
+        """Initial loading of the data.
+        """        
 
         # Load the data.
         # TODO: put directly in folder.
@@ -25,14 +27,26 @@ class TestOTscomics(unittest.TestCase):
     def test_data(self) -> None:
         """Test that the data was loaded properly.
         """
+
+        # Test the dimensions of the data.
         self.assertEqual(self.adata.n_obs, 206)
         self.assertEqual(self.adata.n_vars, 1_000)
 
     def test_cost_matrix(self):
+        """Test that the cost matrix is computed properly.
+        """
+        
+        # Generate the cost matrix C.
         C = otscomics.cost_matrix(
             self.adata.X.T, cost='cosine', normalize_features=True)
+        
+        # Test the dimensions of C.
         self.assertEqual(C.shape, (1_000, 1_000))
+
+        # Test that C has zero diagonal.
         np.testing.assert_almost_equal(np.diag(C), np.zeros(1_000))
+
+        # Test that C has a max of 1.
         np.testing.assert_almost_equal(C.max(), 1)
         
 
