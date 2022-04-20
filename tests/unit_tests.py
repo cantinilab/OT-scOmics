4c05d28981bb8a951888cd626ef0656a8e1fd8cd
Geert-Jan Huizing <huizing@ens.fr>
2022-04-20 17:29:48 +0200

    testing the cost matrix

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -14,7 +14,6 @@ class TestOTscomics(unittest.TestCase):
 
         # Retrieve the clusters.
         clusters = np.array([col.split('_')[-1] for col in data.columns])
-        idx = np.argsort(clusters) # Sorted indices (for visulization)
 
         # Select highly variable genes.
         data = data.iloc[np.argsort(data.std(1))[::-1][:1_000]]
@@ -22,10 +21,18 @@ class TestOTscomics(unittest.TestCase):
         # Converting to AnnData for the rest of the analysis.
         self.adata = ad.AnnData(data.T)
         self.adata.obs['cell_line'] = clusters
+    
+    def test_data(self) -> None:
+        """Test that the data was loaded properly.
+        """
+        self.assertEqual(self.adata.n_obs, 206)
+        self.assertEqual(self.adata.n_vars, 1_000)
 
-
-    def test_nothing(self):
-        self.assertEqual(1, 1)
+    def test_cost_matrix(self):
+        C = otscomics.cost_matrix(
+            self.adata.X.T, cost='cosine', normalize_features=True)
+        self.assertEqual(C.shape, (1_000, 1_000))
+        
 
 if __name__ == '__main__':
     unittest.main()
