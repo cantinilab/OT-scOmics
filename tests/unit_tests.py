fa8cb3bf6cd8efc9849a516171bf9cf2e94e062e
Geert-Jan Huizing <huizing@ens.fr>
2022-04-20 17:56:27 +0200

    added test for c index

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import anndata as ad
 import torch
+from scipy.spatial.distance import cdist
 
 class TestOTscomics(unittest.TestCase):
 
@@ -19,7 +20,7 @@ class TestOTscomics(unittest.TestCase):
         clusters = np.array([col.split('_')[-1] for col in data.columns])
 
         # Select highly variable genes.
-        self.n_vars = 500
+        self.n_obs, self.n_vars = 206, 500
         data = data.iloc[np.argsort(data.std(1))[::-1][:self.n_vars]]
         
         # Converting to AnnData for the rest of the analysis.
@@ -31,10 +32,10 @@ class TestOTscomics(unittest.TestCase):
         """
 
         # Test the dimensions of the data.
-        self.assertEqual(self.adata.n_obs, 206)
+        self.assertEqual(self.adata.n_obs, self.n_obs)
         self.assertEqual(self.adata.n_vars, self.n_vars)
 
-    def test_cost_matrix(self):
+    def test_cost_matrix(self) -> None:
         """Test that the cost matrix is computed properly.
         """
         
@@ -58,7 +59,7 @@ class TestOTscomics(unittest.TestCase):
         # Check positivity.
         self.assertEqual(np.sum(C < 0), 0)
     
-    def test_ot_distance(self):
+    def test_ot_distance(self) -> None:
         """Test the pairwise OT distance.
         """
 
@@ -76,13 +77,36 @@ class TestOTscomics(unittest.TestCase):
             cost='cosine', normalize_features=True)
         
         # Compute the OT distance matrix.
-        D_ot, errors = otscomics.OT_distance_matrix(
+        D_ot, _ = otscomics.OT_distance_matrix(
             data_norm, cost=C, eps=.5,
             n_batches=10, threshold=1e-8,
             max_iter=100, dtype=torch.double, device='cpu')
+
+        # Test that D has the right shape.
+        self.assertEqual(D_ot.shape, (self.n_obs, self.n_obs))
         
-        # Check positivity
+        # Check that D is positive.
         self.assertEqual(np.sum(D_ot < 0), 0)
+
+        # Test that D is symmetric.
+        np.testing.assert_almost_equal(D_ot, D_ot.T)
+
+        # Test that D has zero diagonal.
+        np.testing.assert_almost_equal(np.diag(D_ot), np.zeros(self.n_obs))
+    
+    def test_c_index(self) -> None:
+        """Test the simple C index function
+        """
+
+        # Create an Euclidean distance matrix.
+        D = cdist(self.adata.X, self.adata.X)
+
+        # Compute the C index.
+        score = otscomics.C_index(D, self.adata.obs['celltype'])
+
+        # Check that the score is between 0 and 1.
+        self.assertGreaterEqual(score, 0)
+        self.assertGreaterEqual(1, score)
         
 
 if __name__ == '__main__':
