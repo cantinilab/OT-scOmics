90c8d3de52fbe8f4f7bd56d735df69e1e00b0c47
Geert-Jan Huizing <huizing@ens.fr>
2022-04-22 11:34:14 +0200

    html base url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+html_baseurl = 'otscomics'
+
 
 # -- Options for HTML output -------------------------------------------------
 
